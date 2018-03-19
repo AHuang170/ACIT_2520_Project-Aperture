fbb96b29b5273f70c18c00750f1d5b8255a667f2
robertjanzen <robertjanzenbc@gmail.com>
2018-03-19 12:11:59 -0700

    Add  login, index, 404 pages

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const request = require('request');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
 
 const serverPort = 8080;
-
-
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
-	response.redirect(`http://localhost:${serverPort}/login.html`);
+	response.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/login', (request, response) => {
+	response.sendFile(__dirname + '/public/login.html');
+});
+
+app.use( (request, response) => {
+	response.status(404);
+	response.sendFile(__dirname + '/public/404.html');
+});
 
 app.listen(8080, () => {
     console.log(`Server is up on the port ${serverPort}`);
