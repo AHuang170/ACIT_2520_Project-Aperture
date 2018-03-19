0cc50515c7f2b26c60d9e5662a7ae410c6d64ef6
robertjanzen <robertjanzenbc@gmail.com>
2018-03-19 12:44:40 -0700

    Add steampi call

--- server.js
+++ server.js
@@ -23,4 +23,26 @@ app.use( (request, response) => {
 
 app.listen(8080, () => {
     console.log(`Server is up on the port ${serverPort}`);
+    steam('20').then((result) => {
+    	console.log('Success');
+	}, (errorMessage) => {
+	    console.log(errorMessage);
+	});
 });
+
+var steam = (game_id) => {
+    return new Promise((resolve, reject) => {
+        request({
+            url: 'http://store.steampowered.com/api/appdetails?appids=' + game_id,
+            json: true
+        }, (error, response, body) => {
+            if (error) {
+            	console.log('Error');
+                reject('Cannot connect to Steam Store');
+            } else if (body.status === 'OK') {
+				console.log(body);
+            }
+        });
+    })
+
+};
