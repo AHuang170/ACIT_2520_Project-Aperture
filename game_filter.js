cf9e19c73268f6443a19260611ec6568987fbcd6
AHuang170 <aldrichhuang321@gmail.com>
2018-04-06 15:39:39 -0700

    Update part 1 done

--- game_filter.js
+++ game_filter.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
-const _ = require('lodash');
 const request = require('request');
 
-const old_file = "games.json";
-const new_file = "filtered_games.json";
+const old_file = "updated_list.json";
+const new_file = "filtered_games_3.json";
+
+const start_index = 0;
+const end_index = 10;
 
 var gamelist = fs.readFileSync(old_file);
 var gameobj = JSON.parse(gamelist);
@@ -31,7 +33,7 @@ var steam = (game_id) => {
 
 (async function game_loop(){
   console.log(`Fetching data from ${old_file}, and saving new data to ${new_file}`);
-
+  var index = start_index;
   for (const item of applist){
     console.log(`Fetching data for: ${item.appid} - ${item.name}`);
 
@@ -48,6 +50,10 @@ var steam = (game_id) => {
       }
     }
 
+    index += 1;
+    if (index >= end_index){
+      break;
+    }
   }
   console.log(`\n\n==========Fetching Complete==========\n\n`);
   var game_jsonData = JSON.stringify(newGameList, undefined, 2);
---
+++ update.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const request = require('request');
+
+const raw_file = "games.json";
+const updated_file = "updated_games.json";
+
+const steam_list_api = `https://api.steampowered.com/ISteamApps/GetAppList/v2`;
+
+const filtered_file = `filtered_games.json`;
+
+const new_file = `updated_list.json`;
+
+var base_game_list = fs.readFileSync(raw_file);
+var base_game_obj = JSON.parse(base_game_list);
+var base_app_list = base_game_obj.applist.apps;
+
+var current_list = () => {
+	return new Promise(async(resolve, reject) => {
+		request({
+			url: steam_list_api,
+			json: true
+		}, (error, response, body) => {
+			if(error){
+				reject(error);
+			} else {
+				resolve(body);
+			}
+		});
+	})
+}
+
+var compare_app_lists = (old_obj, current_obj) => {
+	console.log('Comparing Lists');
+	var current_index = 0;
+
+	var old_list = old_obj.applist.apps.slice();
+	var current_list = current_obj.applist.apps.slice();
+
+	var new_obj = {applist: {apps: []}};
+
+	for(var index = 0; index < old_list.length ; index ++){
+		var base_id = old_list[index].appid;
+		var new_id = current_list[current_index].appid;
+		console.log(`Comparing ${base_id} WITH ${new_id}`);
+		if ( base_id != new_id){
+			while(new_id != base_id && current_index < current_list.length){
+				console.log(`Base: ${base_id}, New: ${new_id}`);
+				new_obj.applist.apps.push(current_list[current_index]);
+				current_index += 1;
+				new_id = current_list[current_index].appid;
+			}
+		} 
+		current_index += 1;
+
+	}
+
+	while(current_index < current_list.length){
+		new_obj.applist.apps.push(current_list[current_index]);
+		current_index += 1;
+	}
+	return new_obj;
+}
+
+var main = () => {
+	// current_list().then((result) => {
+	// 	var current_list = Object.assign({}, result);
+	// 	var old_list = JSON.parse(fs.readFileSync(filtered_file));
+
+	// 	var new_obj = compare_app_lists( old_list, current_list);
+
+	// 	fs.writeFileSync(new_file, JSON.stringify(new_obj, undefined, 2));
+
+
+
+
+	// }).catch((error) => {
+	// 	console.log(error);
+	// });
+
+	console.log(JSON.parse(fs.readFileSync(new_file)).applist.apps.length);
+}
+
+main();
