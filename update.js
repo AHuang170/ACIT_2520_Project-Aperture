59e32e9b307805df409e0f9a012d7f69169945cb
AHuang170 <32000451+AHuang170@users.noreply.github.com>
2018-04-08 21:13:22 -0700

    Some Edits

--- update.js
+++ update.js
@@ -8,7 +8,8 @@ const steam_list_api = `https://api.steampowered.com/ISteamApps/GetAppList/v2`;
 
 const filtered_file = `filtered_games.json`;
 
-const new_file = `updated_list.json`;
+const new_file = "filtered_game_AU_new.json";
+const new_non_game_file = "filtered_non_game_AU_new.json";
 
 var base_game_list = fs.readFileSync(raw_file);
 var base_game_obj = JSON.parse(base_game_list);
@@ -62,22 +63,97 @@ var compare_app_lists = (old_obj, current_obj) => {
 }
 
 var main = () => {
-	// current_list().then((result) => {
-	// 	var current_list = Object.assign({}, result);
-	// 	var old_list = JSON.parse(fs.readFileSync(filtered_file));
+	current_list().then((result) => {
+		var current_list = Object.assign({}, result);
+		var old_list = JSON.parse(fs.readFileSync(filtered_file));
 
-	// 	var new_obj = compare_app_lists( old_list, current_list);
+		var new_obj = compare_app_lists( old_list, current_list);
 
-	// 	fs.writeFileSync(new_file, JSON.stringify(new_obj, undefined, 2));
+		var newGameList = {applist: {apps: []}};
+		var nonGameList = {applist: {apps: []}};
+		var applist = new_obj.applist.apps;
 
+		(async function game_loop(){
 
+		  for (var index = 0; index <= applist.length; index++){
 
+		    var item = applist[index];
+		    console.log(`Fetching data for: ${item.appid} - ${item.name}`);
 
-	// }).catch((error) => {
-	// 	console.log(error);
-	// });
+		    try{
+		        var result = await steam(item.appid);
+		    } catch (error) {
+		      console.log(error);
+		    }
 
-	//console.log(JSON.parse(fs.readFileSync(new_file)).applist.apps.length);
+		    if (result != undefined){
+
+		      if (result["type"] == 'game'){
+		        newGameList.applist.apps.push(item);
+		      } else {
+						nonGameList.applist.apps.push(item);
+					}
+		    }
+		  }
+		  console.log(`\n\n==========Fetching Complete==========\n\n`);
+		  var game_jsonData = JSON.stringify(newGameList, undefined, 2);
+		  fs.writeFileSync(new_file, game_jsonData, function(err){
+		    if(err){
+		      console.log(err);
+		    }
+		  })
+
+			var non_game_jsonData = JSON.stringify(nonGameList, undefined, 2);
+			fs.writeFileSync(new_non_game_file, non_game_jsonData, function(err){
+		    if(err){
+		      console.log(err);
+		    }
+		  })
+		})();
+
+	}).catch((error) => {
+		console.log(error);
+	});
+
+}
+
+var steam = (game_id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+
+      request({
+        url: `http://store.steampowered.com/api/appdetails?appids=${game_id}`,
+        json: true
+      }, (error, response, body) => {
+
+        if(error){
+          reject(error);
+        } else {
+
+          if (body != undefined){
+            if (body[game_id] != undefined){
+							if(body[game_id]['success']){
+									return_obj = Object.assign({}, body[game_id].data);
+									resolve(return_obj);
+									console.log(`Type: ${body[game_id].data['type']}`);
+							} else {
+									console.log(`Result: undefined`);
+									resolve(undefined);
+							}
+            } else {
+							console.log(`Result: undefined`);
+              resolve(undefined);
+            }
+          } else {
+						console.log(`Result: undefined`);
+            resolve(undefined);
+          }
+        }
+
+      });
+
+    }, 1100);
+  });
 }
 
 main();
